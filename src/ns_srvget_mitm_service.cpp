fa8ca8099eba4fb7b070192a2706ab873f68e6bd
MasaGratoR <masagrator@gmail.com>
2026-03-05 19:18:56 +0100

    Little oopsie

--- src/ns_srvget_mitm_service.cpp
+++ src/ns_srvget_mitm_service.cpp
@@ -95,13 +95,13 @@ Result isJpegBaseline(const ams::fs::FileHandle file) {
 	uint16_t block_id;
 	uint16_t block_length;
 
-	ptrdiff_t offset = 0;
-
-	ams::fs::ReadFile(file, offset, &block_id, sizeof(block_id));
+	ams::fs::ReadFile(file, 0, &block_id, sizeof(block_id));
 	if (block_id != JPEG_SOI) {
 		return 2;
 	}
 
+	ptrdiff_t offset = 2;
+	
 	while (R_SUCCEEDED(ams::fs::ReadFile(file, offset, &block_id, sizeof(block_id)))) {
 		offset += 2;
 		if (block_id >= JPEG_MARKER_NO_SIZE_BEGIN && block_id <= JPEG_MARKER_NO_SIZE_END) {
